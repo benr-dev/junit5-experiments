c12a2341f67a77d9e581dbc486e581d708715c6a
ben <ben@devbox>
2019-10-22 17:38:05 +0100

    Adds setUp in junit 5 format

--- src/test/java/org/example/TestBowlingScorecard.java
+++ src/test/java/org/example/TestBowlingScorecard.java
@@ -3,19 +3,22 @@
  */
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBowlingScorecard {
-    @Test public void when_initialised_then_noErrors() {
-        new BowlingScorecard();
+
+    private BowlingScorecard scorecard;
+
+    @BeforeEach public void setUp() {
+        scorecard = new BowlingScorecard();
     }
 
     @Test public void when_gutterGame_then_scoreIs0() {
         // given
-        BowlingScorecard scorecard = new BowlingScorecard();
 
         // when
         int score = scorecard.score(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
@@ -27,7 +30,6 @@ public class TestBowlingScorecard {
     @Test
     public void when_all1sGame_then_scoreIs20() {
         // given
-        BowlingScorecard scorecard = new BowlingScorecard();
 
         // when
         int score = scorecard.score(1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1);
@@ -38,7 +40,6 @@ public class TestBowlingScorecard {
 
     @Test public void when_spareFollowedBy1Game_then_scoreIs12() {
         // given
-        BowlingScorecard scorecard = new BowlingScorecard();
 
         // when
         int score = scorecard.score(5,5, 1,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
@@ -49,7 +50,6 @@ public class TestBowlingScorecard {
 
     @Test public void when_strikeFollowedBy1And1Game_then_scoreIs14() {
         // given
-        BowlingScorecard scorecard = new BowlingScorecard();
 
         // when
         int score = scorecard.score(10, 1,1, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
@@ -60,7 +60,6 @@ public class TestBowlingScorecard {
 
     @Test public void when_perfectGame_then_scoreIs300() {
         // given
-        BowlingScorecard scorecard = new BowlingScorecard();
 
         // when
         int score = scorecard.score(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
